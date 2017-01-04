69c5bb4ee29a3b7c36c72cecca3775261a26ac30
rboykock <rboykock@gmail.com>
2017-01-05 00:06:10 +0200

    Initial commit

---
+++ app/src/main/java/rboyko/cherkassy/pro/abetka/FragmentJa.java
@@ -0,0 +1,17 @@
+package rboyko.cherkassy.pro.abetka;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by rost on 04.01.2017.
+ */
+public class FragmentJa extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ja,null);
+    }
+}
